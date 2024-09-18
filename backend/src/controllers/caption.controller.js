cb3e2b9eee4467bd59182bbf48f6d4994840532a
kelly <kellybackwards@gmail.com>
2024-09-18 15:14:01 -0400

    removed certain data

--- backend/src/controllers/caption.controller.js
+++ backend/src/controllers/caption.controller.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: 'postgres',
     host: '127.0.0.1',
     database: 'caption_two',
-    password: 'beepisscum'
+    password: ''
 })
 
 const model = require("../models");
